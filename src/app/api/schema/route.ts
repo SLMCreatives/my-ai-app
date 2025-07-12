f224ead48b5e8b0ff251d784e5cd5431d14ef3bd
SLMCreatives <sulaiman@slmcreatives.com>
2025-07-13 01:36:26 +0800

    schema blog

--- src/app/api/schema/route.ts
+++ src/app/api/schema/route.ts
@@ -1,15 +1,23 @@
-import { generateText } from "ai";
+import { generateObject } from "ai";
 import { openai } from "@ai-sdk/openai";
+import { z } from "zod";
 
 export async function POST(req: Request) {
   const { prompt }: { prompt: string } = await req.json();
 
-  const { text } = await generateText({
+  const { object } = await generateObject({
     model: openai("gpt-4.1-mini"),
+    schema: z.object({
+      title: z.string(),
+      body: z.string(),
+      tags: z.array(z.string()),
+      slug: z.string(),
+      excerpt: z.string()
+    }),
     system:
       "You are a friendly, warm, and engaging blog‐writing assistant for UNITAR's digital marketing team. Your blog should be short and to the point. Avoid using a lot of long paragraphs. Use a creative and engaging tone. Use the following prompt to guide your responses. ",
     prompt
   });
-  // console.log(text);
-  return Response.json({ text });
+  //console.log(object);
+  return Response.json({ object });
 }
--- src/app/blog-writer/page.tsx
+++ src/app/blog-writer/page.tsx
@@ -5,30 +5,41 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { useRef, useState } from "react";
-import { toast } from "sonner";
-import { useChat } from "@ai-sdk/react";
+//import { toast } from "sonner";
+import { Switch } from "@/components/ui/switch";
+import { Badge } from "@/components/ui/badge";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
+type Generation = {
+  title: string;
+  body: string;
+  tags: string[];
+  slug: string;
+  excerpt: string;
+};
+
 export default function BlogPage() {
-  const [generation, setGeneration] = useState("");
+  const [generation, setGeneration] = useState<Generation | string>("");
   const [isLoading, setIsLoading] = useState(false);
   const [hide, setHide] = useState(false);
-  const { messages } = useChat();
+  const [language, setLanguage] = useState("English");
+  //const { messages } = useChat();
 
   const formRef = useRef<HTMLFormElement>(null);
 
   const [data, setData] = useState({
     event_name: "",
-    event_description: "",
     date: "",
     time: "",
     location: "",
-    description: ""
+    description: "",
+    cta: "",
+    language: ""
   });
 
-  const unsecuredCopyToClipboard = (text: string) => {
+  /* const unsecuredCopyToClipboard = (text: string) => {
     const textArea = document.createElement("textarea");
     textArea.value = text;
     document.body.appendChild(textArea);
@@ -40,66 +51,64 @@ export default function BlogPage() {
       console.error("Unable to copy to clipboard", err);
     }
     document.body.removeChild(textArea);
-  };
+  }; */
 
   /**
    * Copies the text passed as param to the system clipboard
    * Check if using HTTPS and navigator.clipboard is available
    * Then uses standard clipboard API, otherwise uses fallback
    */
-  const copyToClipboard = (generation: string) => {
+  /* const copyToClipboard = (generation: string) => {
     if (window.isSecureContext && navigator.clipboard) {
       navigator.clipboard.writeText(generation);
     } else {
       unsecuredCopyToClipboard(generation);
     }
     toast("Copied to clipboard");
-  };
+  }; */
 
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch px-4 md:px-10">
-      <div>
-        <h1 className="text-2xl font-bold">Event Blog Writer</h1>
+    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch justify-start px-4 md:px-10">
+      <div className=" flex flex-col gap-0">
+        <h1 className="text-2xl font-bold">UNITAR Blog Writer</h1>
         {hide === false && (
           <form
             onSubmit={(e) => e.preventDefault()}
-            className="flex flex-col gap-4 py-4"
+            className="flex flex-col gap-4"
             ref={formRef}
           >
-            <p className="mb-4 text-md italic text-white/70">
+            <p className="mb-4 text-md italic">
               Please provide the following details:
             </p>
-            <Label htmlFor="event_name">Event Name</Label>
+            <div className="flex flex-row gap-2 w-full py-2">
+              <div className="flex flex-col gap-4 w-full">
+                <Label htmlFor="date">Date</Label>
+                <Input
+                  id="date"
+                  type="date"
+                  value={data.date}
+                  onChange={(e) => setData({ ...data, date: e.target.value })}
+                />
+              </div>
+              <div className="flex flex-col gap-4 w-full">
+                <Label htmlFor="time">Time</Label>
+                <Input
+                  id="time"
+                  type="time"
+                  value={data.time}
+                  onChange={(e) => setData({ ...data, time: e.target.value })}
+                />
+              </div>
+            </div>
+            <Label htmlFor="event_name">Title / Name</Label>
             <Input
               id="event_name"
               type="text"
               value={data.event_name}
               onChange={(e) => setData({ ...data, event_name: e.target.value })}
             />
-            <Label htmlFor="event_description">Event Brief</Label>
-            <Input
-              id="event_description"
-              type="text"
-              value={data.event_description}
-              onChange={(e) =>
-                setData({ ...data, event_description: e.target.value })
-              }
-            />
-            <Label htmlFor="date">Date</Label>
-            <Input
-              id="date"
-              type="date"
-              value={data.date}
-              onChange={(e) => setData({ ...data, date: e.target.value })}
-            />
-            <Label htmlFor="time">Time</Label>
-            <Input
-              id="time"
-              type="time"
-              value={data.time}
-              onChange={(e) => setData({ ...data, time: e.target.value })}
-            />
-            <Label htmlFor="description">Description</Label>
+
+            <Label htmlFor="description">Brief</Label>
             <Textarea
               id="description"
               value={data.description}
@@ -107,6 +116,25 @@ export default function BlogPage() {
                 setData({ ...data, description: e.target.value })
               }
             />
+            <Label htmlFor="cta">CTA</Label>
+            <Input
+              id="cta"
+              type="text"
+              value={data.cta}
+              onChange={(e) => setData({ ...data, cta: e.target.value })}
+            />
+            <Label htmlFor="Language">Language</Label>
+            <div className="flex flex-row gap-2">
+              <Switch
+                id="Language"
+                value="English"
+                checked={language === "English"}
+                onCheckedChange={(checked) =>
+                  setLanguage(checked ? "English" : "Bahasa Melayu")
+                }
+              />
+              <Label htmlFor="Language">{language}</Label>
+            </div>
             <div className="flex flex-row gap-4 fixed bottom-10 left-1/2 transform -translate-x-1/2">
               <Button
                 type="submit"
@@ -115,19 +143,20 @@ export default function BlogPage() {
                 disabled={isLoading}
                 onClick={async () => {
                   setIsLoading(true);
-                  await fetch("/api/blog", {
+                  await fetch("/api/schema", {
                     method: "POST",
                     body: JSON.stringify({
                       prompt:
-                        `Event Name: ${data.event_name}` +
+                        `Blog Title: ${data.event_name}` +
                         `When: ${data.date} ${data.time}` +
-                        `Location: ${data.location}` +
-                        `Description: ${data.description}`
+                        `Brief: ${data.description}` +
+                        `CTA: ${data.cta}` +
+                        `Language: ${language}`
                     })
                   }).then((response) => {
                     response.json().then((json) => {
                       setHide(true);
-                      setGeneration(json.text);
+                      setGeneration(json.object);
                       setIsLoading(false);
                     });
                   });
@@ -142,28 +171,15 @@ export default function BlogPage() {
                 disabled={isLoading}
                 onClick={async () => {
                   setIsLoading(true);
-                  const fakedata = {
-                    event_name: "",
-                    event_description: "",
-                    date: `${new Date().toLocaleDateString()}`,
-                    time: `${new Date().toLocaleTimeString()}`,
-                    location: "UNITAR Main Campus",
-                    description:
-                      "Make up a funny story that happened. Make it into a learnable lesson for uni students."
-                  };
-                  await fetch("/api/blog", {
+                  await fetch("/api/schema", {
                     method: "POST",
                     body: JSON.stringify({
-                      prompt:
-                        `Event Name: ${fakedata.event_name}` +
-                        `When: ${fakedata.date} ${fakedata.time}` +
-                        `Location: ${fakedata.location}` +
-                        `Description: ${fakedata.description}`
+                      prompt: `Write a student lifestyle blog post for UNITAR's digital marketing team. The blog should be engaging, creative, and concise`
                     })
                   }).then((response) => {
                     response.json().then((json) => {
                       setHide(true);
-                      setGeneration(json.text);
+                      setGeneration(json.object);
                       setIsLoading(false);
                     });
                   });
@@ -175,27 +191,44 @@ export default function BlogPage() {
           </form>
         )}
         {generation !== "" && (
-          <div className="flex flex-col gap-4">
-            <div className="space-y-4">
-              {messages.map((m) => (
-                <div key={m.id} className="whitespace-pre-wrap">
-                  <div>
-                    <div className="font-bold">{m.role}</div>
-                    <p>{m.content}</p>
-                  </div>
+          <div className="flex flex-col gap-4 py-4">
+            {typeof generation === "object" ? (
+              <div className="flex flex-col gap-4">
+                <Label className="text-sm">Title</Label>
+                <h2 className="text-xl font-bold">{generation.title}</h2>
+                <Label className="text-sm">Slug</Label>
+                <Input
+                  type="url"
+                  className="w-full"
+                  value={`unitar.my/${generation.slug}`}
+                  readOnly
+                />
+                <Label className="text-sm">Excerpt</Label>
+                <Textarea
+                  className="w-full"
+                  value={generation.excerpt}
+                  readOnly
+                />
+                <Label className="text-sm">Tags</Label>
+                <div className="flex flex-row gap-2 flex-wrap">
+                  {generation.tags.map((tag, index) => (
+                    <Badge key={index}>{tag}</Badge>
+                  ))}
                 </div>
-              ))}
-            </div>
-            <div className="py-4 px-2 rounded-md ">
-              <p className="whitespace-pre-wrap prose">{generation}</p>
-            </div>
+                <Label className="text-sm">Body</Label>
+                <p className="text-xl prose">{generation.body}</p>
+              </div>
+            ) : (
+              <h2 className="text-xl font-bold">Generated Blog Post</h2>
+            )}
+
             <div className="flex flex-row gap-2 py-4 fixed bottom-10 left-1/2 transform -translate-x-1/2">
-              <Button
+              {/*  <Button
                 variant={"default"}
                 onClick={() => copyToClipboard(generation)}
               >
                 Copy
-              </Button>
+              </Button> */}
               <Button
                 onClick={() => {
                   setGeneration("");
